951eef05150ffd8a7851af7135e138751b5880db
Yari-Ros <2118088@talnet.nl>
2019-11-20 10:13:41 +0100

    Uitwerking van de exercise

---
+++ klaslokaal.php
@@ -0,0 +1,17 @@
+<?php
+$Class = array (
+    "Yari",
+    "Kenza",
+    "Youssef",
+    "Leah",
+    "Jim",
+    "Kevin",
+    "Beau",
+    "Riccardo",
+    "Jimmy",
+    "George",
+    "Zanhua Li",
+    "Izdin",
+);
+
+var_dump ($Class) ;
